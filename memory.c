9ab316674432c57128233e09b378fbd55914e206
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-07-08 22:22:17 +0100

    Finish common instructions

---
+++ memory.c
@@ -0,0 +1,18 @@
+//
+// Created by Maksim Rukomoynikov on 08/07/2025.
+//
+
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+  if (newSize == 0) {
+    free(pointer);
+    return NULL;
+  }
+
+  void* result = realloc(pointer, newSize);
+  if (result == NULL) exit(1);
+  return result;
+}
