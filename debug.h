f124d6e9eafe74f8a8a8cf1d463b45d476a8f7b9
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-07-09 21:02:12 +0100

    Add debugging functionality for chunk disassembly

---
+++ debug.h
@@ -0,0 +1,13 @@
+//
+// Created by Maksim Rukomoynikov on 09/07/2025.
+//
+
+#ifndef clox_debug_h
+#define clox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
